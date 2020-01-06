eee734e66d6adec5693d0bf63ed2f83e1b68a953
gabsrodriguez <gmederos95@gmail.com>
2020-01-06 17:08:52 +0000

    clear input field

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,26 +5,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	const [toDo, setToDO] = useState("");
+	const [value, setValue] = useState();
+	// const [toDo, setToDO] = useState();
 	const [newToDo, setNewToDo] = useState([]);
 
-	function toDoChange(e) {
-		setToDO(e.target.value);
-	}
+	// function toDoChange(e) {
+	// 	setToDO(e.target.value);
+	// }
 
 	function addToDo(e) {
-		if (toDo === "") return;
-		setNewToDo([...newToDo, { text: toDo }]);
+		if (value === "") return;
+		setNewToDo([...newToDo, { text: value }]);
+		setValue("");
 	}
 
 	return (
 		<>
 			<h1>To Do</h1>
-
-			<input placeholder="add to list" onChange={toDoChange} />
-
+			<input
+				id="input1"
+				type="text"
+				placeholder="add to list"
+				onChange={e => setValue(e.target.value)}
+				value={value}
+			/>
 			<button onClick={addToDo}>add</button>
-
 			<ul>
 				{newToDo.map(todo => (
 					<li key={todo.index}>{todo.text}</li>
